1813a474236dc437e445d2017d38bebec3a899d4
Dhanushiya26 <dhanushiyaa26@gmail.com>
2025-07-29 02:05:56 -0700

    Add files via upload

---
+++ Task 1 SMART LIGHT CONTROL.ino
@@ -0,0 +1,24 @@
+﻿String command = "";
+
+void setup() {
+  pinMode(13, OUTPUT); // LED pin
+  Serial.begin(9600);
+  Serial.println("Type ON or OFF to control LED");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.readStringUntil('\n');
+    command.trim(); // remove any whitespace
+
+    if (command == "ON") {
+      digitalWrite(13, HIGH);
+      Serial.println("LED is ON");
+    } else if (command == "OFF") {
+      digitalWrite(13, LOW);
+      Serial.println("LED is OFF");
+    } else {
+      Serial.println("Invalid command. Use ON or OFF.");
+    }
+  }
+}
